bca55624e33ee9e313864c5e382467fbb63358c6
georgemibu <georgemibu@gmail.com>
2025-05-02 18:35:00 -0300

    Inicio proyecto

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Receta from './components/Receta.jsx'
 
 function App() {
 
   return (
     <>
       <h1>Restaurant</h1>
+      <Receta />
     </>
   )
 }
---
+++ components/Receta.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import './Receta.css'; // Asegúrate de tener este archivo con el CSS que copiaste
+
+const Receta = () => {
+  return (
+    <div className="menu">
+      <div className="nav-receta">
+        <img src="/assets/atras.svg" alt="Atrás" />
+        <img src="/assets/tres-puntos.svg" alt="Opciones" />
+        <img src="/assets/menu.svg" alt="Menú" />
+      </div>
+
+      <div className="foto-receta">
+        <img src="/assets/pollo-asado-horno-patatas-cebolla.jpg" alt="Foto receta" />
+      </div>
+
+      <div className="descripcion-receta">
+        <h1 className="titulo-receta">Pollo al horno con papas fritas</h1>
+        <h2 className="precio-receta">$10.00</h2>
+      </div>
+
+      <div className="detalle-receta">
+        <p className="detalle">
+          Pollo dorado al horno con hierbas aromáticas, acompañado de papas rústicas crujientes, cocidas lentamente para lograr un equilibrio perfecto entre sabor, textura y aroma casero.
+        </p>
+      </div>
+
+      <div className="valoracion-receta">
+        <div className="valoracion-img-container">
+          <img src="/assets/star.svg" alt="Valoración" />
+          <p>4.0</p>
+        </div>
+        <div className="valoracion-img-container">
+          <img src="/assets/fuego.png" alt="Kcal" />
+          <p>600 kcal</p>
+        </div>
+        <div className="valoracion-img-container">
+          <img src="/assets/despertador.png" alt="Tiempo de cocción" />
+          <p>20 min</p>
+        </div>
+      </div>
+
+      <div className="ingredientes-receta">
+        <h3>Ingredientes</h3>
+        <div className="ingredientes">
+          <div className="img-container">
+            <img src="/assets/Nueva carpeta/pollo.png" alt="Pollo" />
+          </div>
+          <div className="img-container">
+            <img src="/assets/Nueva carpeta/patata.png" alt="Patata" />
+          </div>
+          <div className="img-container">
+            <img src="/assets/Nueva carpeta/cebolla.png" alt="Cebolla" />
+          </div>
+          <div className="img-container">
+            <img src="/assets/Nueva carpeta/manzana.png" alt="Manzana" />
+          </div>
+        </div>
+      </div>
+
+      <div className="agregar-receta-container">
+        <div className="contador-cantidad">
+          <button id="menos">-</button>
+          <p>0</p>
+          <button id="mas">+</button>
+        </div>
+        <div className="add-receta-container">
+          <button className="add-to-cart">
+            <img src="/assets/carrito-de-compras.svg" alt="Agregar al carrito" />
+            <p>AÑADIR AL CARRITO</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Receta;
